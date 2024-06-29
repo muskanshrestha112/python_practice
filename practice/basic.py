a35c89354fd7a9ae5f92cfb2603a07e6dfbf8a08
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-06-29 22:32:34 +0545

    calculater finish

---
+++ practice/basic.py
@@ -0,0 +1,89 @@
+# print("hello World!")
+# print('hello World!')
+
+'''my name is muskan'''
+#hello
+# if 8<10:
+#     print('eight is smaller')
+# if 3<5:
+#     print('print five is greater')
+# if 3<5:
+#     print('three is smaller')
+# x=5
+# print(x)
+#this is comment
+# print("hello world")
+# print('hello') #comment
+# print('5')
+# '''this is comment'''
+# y=4
+# y="muskan"
+# print(y)
+# x=float(3)
+# print(x)
+# y=5
+# print(type(y))
+# a=3
+# A='mus'
+# print(A)
+# my_var='mus'
+# print(my_var)
+'''myVariablename='mus'
+print(myVariablename)'''
+# x,y='man','women'
+# print(y)
+# print(x)
+# x=z='monkey'
+# print(x)
+# print(z)
+# play=['ball','you']
+# x,y=play
+# print(x)
+# # print(y)
+# x = "hot"
+# y = "pot"
+# print(x,y)
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+# x = "hot"
+# y = "pot"
+# print(x+y)
+# x=6
+# y=8
+# print(x+y)
+# x=5
+# y='mus'
+# print(x,y)
+# x='mus'
+# def myfun():
+#     x='sha'
+#     print("my name is" +x)
+
+# myfun()    
+# print("my name is" +x)
+# x='pick'
+# def myfun():
+#     global x
+# x='slick'
+    
+# myfun()
+# print('my name' +x)
+# import random
+
+# # print(random.randrange(1, 10))
+# a='momo'
+# print(a[2])
+# for x in'mummy':
+#     print(len(x))
+# txt='i have you'
+# print('to' in txt)
+# a='mommy'
+# print(a[1:3])
+x=1
+y=''
+print(bool(x))
+print(bool(y))
+
+
---
+++ project/calculater.py
@@ -0,0 +1,65 @@
+
+# num1=float(input("Enter a number: "))
+# num2=float(input("Enter a number: "))
+
+# print(f"Total: {num1+num2}")
+# print(f"Total: {num1-num2}")
+# print(f"Total: {num1*num2}")
+# print(f"Total: {round(num1/num2,4)}")
+# print(f"Total: {num1**2}")
+# print(f"Total: {num1//num2}")
+
+# def Add(a,b):
+#     print(a+b)
+
+# def Sub(a,b):
+#     print(a-b)
+
+# def Mul(a,b):
+#     print(a*b)
+
+# def Div(a,b):
+#     print(a/b)
+
+
+# Sub(5,3)
+# Add(2,3)
+
+# def Calcu(a,b):
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a/b)
+    
+# # Calcu(5,9)
+
+# def Calcu(a,b,opt):
+#     if opt == '+':
+#         return a+b
+#     elif opt == '-':
+#         return a-b
+#     else :
+#         return "enter valid operator"
+    
+
+# print(Calcu(4,6,"a"))
+
+def Show(name,age,money):
+    if age < 18:
+        return "you are not allowed"
+    
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+# username = input("Enter username:")
+# print("Username is: " + username)
