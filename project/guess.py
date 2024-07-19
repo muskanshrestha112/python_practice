bb3fe7bb48ccab75b1f2662df20490b1fdb2e2a7
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-07-19 15:57:50 +0545

    guessnumber

--- project/guess.py
+++ project/guess.py
@@ -1,13 +1,22 @@
 import random
-num = random.randint(0,100)
-guess = 0 
-while guess != num:
+num = random.randint(0,30)
+guess = 0
+i = 0
+while i < 3 :    
     guess = int(input("Enter a number:"))
+    if guess > 30:
+        print('Input number less then 30 ')
+        break
     if (guess < num):
-        print("Pick higher number!")
+         print("Pick higher number!")
     elif (guess > num):
         print("Pick lower number!")
-    else:
-        print("Congrats!!!You have guessed the right number.")   
-        break     
+    else:         
+        print("Congrats!!!You have guessed the right number.") 
+    i += 1
+    print(f"You have {3 - i} guess left")
 
+
+
+
+# while guess != num  : 
