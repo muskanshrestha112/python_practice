0533b4c17edb8702cd1a62fd2a10e81d26d123e6
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-07-19 23:39:24 +0545

    guessgame

--- project/guess.py
+++ project/guess.py
@@ -1,10 +1,19 @@
 import random
-num = random.randint(0,30)
-guess = 0
+from os import system as muskan
+
+
+muskan('cls')
+num = random.randint(0,10)
+print(num)
 i = 0
-while i < 3 :    
-    guess = int(input("Enter a number:"))
-    if guess > 30:
+while i < 3 : 
+    try:   
+        guess = int(input("Enter a number:"))
+    except Exception as e:
+        print("Can only enter number !!!")
+        print("ERROR",e)
+        break
+    if guess > 10:
         print('Input number less then 30 ')
         break
     if (guess < num):
@@ -13,8 +22,11 @@ while i < 3 :
         print("Pick lower number!")
     else:         
         print("Congrats!!!You have guessed the right number.") 
+        break
     i += 1
     print(f"You have {3 - i} guess left")
+
+
 
 
 
