bd2e8d18d14e6c0037bd0aabc3610b799f362124
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-07-21 22:39:17 +0545

    lambda basic

---
+++ practice/lamda.py
@@ -0,0 +1,20 @@
+# a = [1, 3, 5, 7, 8, 9, 2]
+
+# b = list(filter()) 
+a, b = 5, 7
+
+# def my_func(a, b):
+#     return a + b
+
+# print(my_func(a, b))
+
+# add_fun = lambda a, b: a + b
+# print(add_fun(a, b))     
+
+# def my_func(a):
+#     return a*2
+
+# print(my_func(a))
+
+add_fun = lambda a: a*2
+print(add_fun(a))   
