e107a073cabb53481f8b9ad393ceea0fc3f1fb47
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-07-29 00:03:32 +0545

    dic start

---
+++ practice/dictionary.py
@@ -0,0 +1,64 @@
+tiktak = {
+    "keys": "values",
+    "Name": "Muskan", 
+    "Age": 20,
+    "Address": "Maitidevi",
+    "Love": "Tarun",
+    "phone": 9854678900,
+    "Sidechicks": ["Radhe", "Asmita", "Manju"]
+    }
+
+
+tiktak["hobbies"] = "dance"
+print(tiktak)
+
+# list1 = ["Name", "Age", "Address"]
+# list2 = ["Muskan", "twenty", "maitidevi"]
+
+# print(set(list2))
+# print(type(list1))
+
+
+# {}{}[][[]]()
+# for x in tiktak.values(): # to print values
+    # print(x)
+
+# for x in tiktak: # to print keys
+#     print(x)
+
+"""
+for multiple paragraph commenting 
+"""
+
+# for x, y in tiktak.items():
+#     print(x, y)
+
+
+
+# del tiktak["phone"]
+# print(tiktak)
+# print(tiktak["Sidechicks"])
+
+
+# print(tiktak.get("phone", "phone does not exsits"))
+
+# if "phone" in tiktak:
+#     print(tiktak["phone"])
+# else:
+#     print("Phone does not exists")
+
+# print
+
+
+# # print(tiktak["Sidechicks"])
+# print(tiktak["Sidechicks"][1])
+
+# print(type(tiktak))
+# print(tiktak["Age"])
+# a = tiktak.keys()
+# print(a)
+
+
+
+# toktok = dict(name = "Tarun", age = 50, occupation = "my bf")
+# print(toktok.keys())
