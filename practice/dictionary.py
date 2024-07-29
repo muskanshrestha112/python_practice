fa70ef0c74dcf9c9a9792098ea2de7882d8530bf
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-07-29 20:54:05 +0545

    nested dict

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -8,12 +8,89 @@ tiktak = {
     "Sidechicks": ["Radhe", "Asmita", "Manju"]
     }
 
+# first = {
+#     "name" : "Ram",
+#     "class" : 9
+#         }
 
-tiktak["hobbies"] = "dance"
-print(tiktak)
+# second = {
+#     "name" : "Sita",
+#     "class" : 9
+# }
 
-# list1 = ["Name", "Age", "Address"]
-# list2 = ["Muskan", "twenty", "maitidevi"]
+# third = {
+#     "name" : "lok",
+#     "class" : 9
+# }
+
+# rank = {
+#     "first" : first,
+#     "second" : second,
+#     "third" : third
+# }
+
+rank = {
+    "first" : {
+    "name" : "Ram",
+    "class" : 9
+},
+
+    "second" : {
+    "name" : "Sita",
+    "class" : 9
+},
+
+    "third" : {
+    "name" : "lok",
+    "class" : 9
+}
+}
+
+
+# print(rank["first"]["class"]) # individual item access
+# print(rank["second"])
+
+# loop in nested dict
+# for x, obj in rank.items():
+#     print(x)
+
+#     for y in obj:
+#         print(y + ':', obj[y])
+
+
+# tiktak.update({"Age": 21}) # to update the values
+# print(tiktak)
+
+# tuktuk = tiktak.copy() # to make a copy of dic
+# tuktuk = dict(tiktak)
+# print(tuktuk)
+
+
+
+
+
+
+
+# tiktak["hobbies"] = "dance" #add elements
+# print(tiktak)
+
+
+list1 = ["Name", "Age", "Address"]
+list2 = ["Muskan", "twenty", "maitidevi"]
+mylist = dict(zip(list1, list2))
+print(mylist)
+for x in mylist.items():
+    print(x)
+
+
+
+
+
+# mylist["list1"] = ["Name", "Age", "Address"]
+# mylist["list2"] = ["Muskan", "twenty", "maitidevi"]
+# print(mylist)
+
+
 
 # print(set(list2))
 # print(type(list1))
@@ -47,7 +124,7 @@ for multiple paragraph commenting
 # else:
 #     print("Phone does not exists")
 
-# print
+
 
 
 # # print(tiktak["Sidechicks"])
