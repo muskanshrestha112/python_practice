fd69c2c0a9964b9d9d4ce8db66fa003d048e739d
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-08-04 15:38:24 +0545

    tip_billing

--- practice/tipbill.py
+++ practice/tipbill.py
@@ -1,17 +1,16 @@
+total_bill = 0
 
 tip = 0
 bill = input("Enter the total bill:")
 tip = input("Enter the tip:")  
   
-if tip == 10:
-    tip += 10
-    bill = float(bill) + ((tip/100) * float (bill))
-elif tip == 20:
-    tip += 20
-    bill = float(bill) + ((tip/100) * float (bill))
-    print(bill)
+if tip == "10":
+    total_bill = float(bill) + ((10/100) * float (bill))
+elif tip == "20":
+    total_bill = float(bill) + ((20/100) * float (bill))
+print(total_bill)
 people = input("number of people:")
-splitbill = float(bill) / float (people)
+splitbill = float(total_bill) / float (people)
 print(splitbill)
 
 
