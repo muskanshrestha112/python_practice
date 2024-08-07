6f62f161de4dcfb3fac508a84c030dfe56e14d5f
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-08-07 10:33:59 +0545

    calc_practice

---
+++ practice/calc.py
@@ -0,0 +1,28 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the seconf number:"))
+
+print("1) Add")
+print("2) Sub")
+print("3) Mul")
+print("4) Div")
+
+opr = (input("Choose a option from the above operation:"))
+
+if opr == "1":
+    opr = num1 + num2
+    print(f"The sum = {opr}")
+elif opr == "2":
+    opr = num1 - num2
+    print(f"The difference = {opr}")
+elif opr == "3":
+    opr = num1 * num2
+    print(f"The product ={opr}")    
+elif opr == "4":
+    opr = num1 / num2
+    print(f"The divident ={opr}")    
+else : 
+    print("The number is invalid")
+
+
+
+
