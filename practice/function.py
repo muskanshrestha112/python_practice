8cccf1ba9830f012925755ae2bde56c9994d641e
muskanshrestha112 <shresthamuskan57@gmail.com>
2024-08-26 21:52:45 +0545

    set_split

--- practice/function.py
+++ practice/function.py
@@ -1,8 +1,11 @@
-a = "My name is Muskan"
-b = "tarun"
+# a = "My name is Muskan"
+# b = "tarun"
+# c = "What are you doing?"
 
+# print(c[13:18])
+# print(c[-2:12:-1])
 
-print(b[::-1])
+# print(b[::-1])
 
 # temp = a[0:10]
 # print(temp, b)
@@ -29,3 +32,22 @@ print(b[::-1])
 
 # print(mylist[0])
 
+
+
+common = []
+unique = []
+first = "I love football"
+second = "I dont love football"
+
+for i in second.split():
+    if i not in first.split():
+        unique.append(i)
+    else:
+        common.append(i)
+    
+print(common)
+print(unique)    
+
+
+
+# print(num.split("-"))
